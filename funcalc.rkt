5416ae8a96578d0ffea4e0a8f0290491e339d0a1
Florian Biermann <fbie@itu.dk>
2017-07-15 16:58:02 -0400

    Rename σ to s and sheet to σ

--- funcalc.rkt
+++ funcalc.rkt
@@ -16,7 +16,7 @@
      (e = e)
      (IF e e e))
   ;; A sheet whose cells reduce from expressions to values.
-  (σ ::= (sheet (ca := e) ...)))
+  (s ::= (σ (ca := e) ...)))
 
 (define-extended-language mini-calc-S mini-calc
   (E ::=
@@ -28,11 +28,11 @@
      (IF E e e))
   (cv ::= (ca := v))
   (ce ::= (ca := e))
-  (S ::= (sheet cv ... (ca := E) ce ...)))
+  (S ::= (σ cv ... (ca := E) ce ...)))
 
 (define ->mini-calc
   (reduction-relation mini-calc-S
-   #:domain σ
+   #:domain s
    (--> (in-hole S (n_1 + n_2))
         (in-hole S ,(+ (term n_1) (term n_2)))
         plus)
@@ -53,30 +53,30 @@
         if-ff)
 
    ;; Cell-reference look-up: reference already computed.
-   (--> (sheet cv_1 ... (ca_r := v) cv_2 ... (ca := (in-hole E ca_r)) ce_4 ...)
-        (sheet cv_1 ... (ca_r := v) cv_2 ... (ca := (in-hole E v))    ce_4 ...)
+   (--> (σ cv_1 ... (ca_r := v) cv_2 ... (ca := (in-hole E ca_r)) ce_4 ...)
+        (σ cv_1 ... (ca_r := v) cv_2 ... (ca := (in-hole E v))    ce_4 ...)
         ref-v)
 
    ;; Cell-reference look-up: reference not yet computed, sort one step!
-   (--> (sheet cv_1 ... (ca := (in-hole E ca_r)) ce_2 ... (ca_r := e) ce_3 ...)
-        (sheet cv_1 ... (ca_r := e) (ca := (in-hole E ca_r)) ce_2 ... ce_3 ...)
+   (--> (σ cv_1 ... (ca := (in-hole E ca_r)) ce_2 ... (ca_r := e) ce_3 ...)
+        (σ cv_1 ... (ca_r := e) (ca := (in-hole E ca_r)) ce_2 ... ce_3 ...)
         ref-e)
 
    ;; Cell-reference look-up: referenced cell is empty.
-   (--> (sheet (ca_1 := v) ... (ca := (in-hole E ca_r)) (ca_2 := e) ...)
-        (sheet (ca_1 := v) ... (ca := (err "#EMPTY"))   (ca_2 := e) ...)
+   (--> (σ (ca_1 := v) ... (ca := (in-hole E ca_r)) (ca_2 := e) ...)
+        (σ (ca_1 := v) ... (ca := (err "#EMPTY"))   (ca_2 := e) ...)
         (side-condition (not (member (term ca_r) (append (term (ca_1 ...)) (term (ca_2 ...))))))
         ref-empty)))
 
 
-(define s1 (term (sheet ((rc 1 1) := (1 + 1)))))
-(define s2 (term (sheet ((rc 1 1) := (1 + 1))         ((rc 1 2) := (0 = 2)))))
-(define s3 (term (sheet ((rc 1 1) := (1 + 1))         ((rc 1 2) := (0 = 2)) ((rc 2 1) := (IF (1 = 2) 0 1)))))
-(define s4 (term (sheet ((rc 1 1) := ((1 + 1) + (rc 1 2))) ((rc 1 2) := 42))))
-(define s5 (term (sheet ((rc 1 1) := 42)              ((rc 1 2) := (rc 1 1)))))
-(define s6 (term (sheet ((rc 1 1) := ((rc 1 2) + 1))  ((rc 1 2) := 42))))
-(define s7 (term (sheet ((rc 1 1) := ((rc 1 2) + 1))  ((rc 1 2) := (41 + 1)))))
-(define s8 (term (sheet ((rc 1 1) := ((rc 1 3) + 1))  ((rc 1 2) := (41 + 1)))))
+(define s1 (term (σ ((rc 1 1) := (1 + 1)))))
+(define s2 (term (σ ((rc 1 1) := (1 + 1))         ((rc 1 2) := (0 = 2)))))
+(define s3 (term (σ ((rc 1 1) := (1 + 1))         ((rc 1 2) := (0 = 2)) ((rc 2 1) := (IF (1 = 2) 0 1)))))
+(define s4 (term (σ ((rc 1 1) := ((1 + 1) + (rc 1 2))) ((rc 1 2) := 42))))
+(define s5 (term (σ ((rc 1 1) := 42)              ((rc 1 2) := (rc 1 1)))))
+(define s6 (term (σ ((rc 1 1) := ((rc 1 2) + 1))  ((rc 1 2) := 42))))
+(define s7 (term (σ ((rc 1 1) := ((rc 1 2) + 1))  ((rc 1 2) := (41 + 1)))))
+(define s8 (term (σ ((rc 1 1) := ((rc 1 3) + 1))  ((rc 1 2) := (41 + 1)))))
 
 (test-equal (redex-match? mini-calc σ s1) #t)
 (test-equal (redex-match? mini-calc σ s2) #t)
