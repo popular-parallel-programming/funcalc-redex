653d21501376277a58a7bc4d1ee352c303ef532e
Florian Biermann <fbie@itu.dk>
2017-07-21 09:40:30 +0200

    Update test case

--- lift.rkt
+++ lift.rkt
@@ -67,4 +67,4 @@
 (test-equal (redex-match? λ-calc-L c s1) #t)
 (test-->> lift s2 (term (lft ((rc 1 1) : (rc 2 2)) := (MAP (λ (x x1) (x + x1))
                                                            ((rc 3 3) : (rc 4 4))
-                                                           ((rc 3 5) : (rc 4 5))))))
+                                                           (HUNFOLD ((rc 3 5) : (rc 4 5)) 2)))))
